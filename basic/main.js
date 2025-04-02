d68e63064456a0c8609f3ccd13cd71afa86c4d24
tiagosousa <tiagosousa.tams@hotmail.com>
2025-04-02 15:50:53 +0100

    first steps with three js

---
+++ basic/main.js
@@ -0,0 +1,21 @@
+import * as THREE from 'three';
+
+// Create a scene
+const scene = new THREE.Scene();
+scene.background = new THREE.Color('#F0F0F0')
+
+// Add the camera
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+camera.position.z = 5;
+
+//create and add a cube object
+const geometry = new THREE.BoxGeometry()
+const material = new THREE.MeshLambertMaterial({color: '#468585', emissive: '#468585'})
+
+const cube = new THREE.Mesh(geometry, material)
+scene.add(cube);
+
+// Add lights
+const light = new THREE.DirectionalLight(0x9CDBA6, 10)
+light.position.set(1, 1, 1)
+scene.add(light)
