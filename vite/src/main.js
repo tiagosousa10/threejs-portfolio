5d21060506e5ab66145bc97256f666c668837f57
tiagosousa <tiagosousa.tams@hotmail.com>
2025-04-02 17:54:23 +0100

    creating and testing different animations and figures

--- vite/src/main.js
+++ vite/src/main.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import {OrbitControls} from 'three/addons/controls/OrbitControls.js'
 
 const canvas = document.getElementById('canvas')
 
@@ -11,3 +12,50 @@ const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerH
 camera.position.z=5;
 
 //3. object
+const geometry = new THREE.DodecahedronGeometry();
+const material= new THREE.MeshBasicMaterial({color: '#468565'})
+
+const dodecahedron = new THREE.Mesh(geometry, material)
+
+const boxGeometry = new THREE.BoxGeometry(2,0.1,2);
+const boxMaterial= new THREE.MeshBasicMaterial({color: '#b4b4b3'});
+const box = new THREE.Mesh(boxGeometry, boxMaterial)
+box.position.y=-1.5;
+
+scene.add(dodecahedron)
+scene.add(box)
+
+//4. ADD LIGHTS
+const light = new THREE.SpotLight(0x006769, 100)
+light.position.set(1, 1, 1)
+scene.add(light)
+
+//5. Renderer
+const renderer = new THREE.WebGLRenderer({canvas})
+renderer.setSize(window.innerWidth, window.innerHeight)
+renderer.setPixelRatio(window.devicePixelRatio)
+
+//6. add orbitcontrols
+const controls = new OrbitControls(camera, renderer.domElement)
+controls.enableDamping = true;
+controls.dampingFactor = 0.05;
+controls.enableZoom = true;
+controls.enablePan = true;
+
+//7. animate
+function animate() {
+  requestAnimationFrame(animate)
+
+  dodecahedron.rotation.x += 0.01
+  dodecahedron.rotation.y += 0.01
+
+  box.rotation.y += 0.005;
+
+  controls.update()
+
+  renderer.render(scene, camera)
+
+}
+
+
+animate()
