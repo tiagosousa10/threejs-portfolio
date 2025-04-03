620a91a925df09081a23e5b0e1484ba7425563f7
tiagosousa <tiagosousa.tams@hotmail.com>
2025-04-03 10:41:03 +0100

    building the navbar and constants file added to  map the items needed

--- threejs-portfolio/src/sections/Navbar.jsx
+++ threejs-portfolio/src/sections/Navbar.jsx
@@ -1,6 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+const NavItems = () => {
+   return(
+      <ul className='nav-ul'>
+         {["Home", "About", "Projects", "Contact"].map((item, index) => (
+            <li key={index} className='nav-li'>
+               <a href="/">{item}</a>
+            </li>
+         ))}
+      </ul>
+   )
+}
+
 
 const Navbar = () => {
+   const [isOpen, setIsOpen] = useState(false)
+
+   const toggleMenu = () => {
+      setIsOpen((prevIsOpen) => !prevIsOpen)
+   }
+
   return (
     <header className='fixed top-0 left-0 right-0 z-50 bg-black/90'>
       <div className='max-w-7xl mx-auto '>
@@ -9,9 +28,17 @@ const Navbar = () => {
             TIAGO
             </a>
 
-            <button>
-               <img src={"assets/menu.svg"} alt="toggle" className='w-6 h-6' />
+            <button
+               onClick={toggleMenu}
+               className='text-neutral-400 hover:text-white focus:outline-none sm:hidden flex'
+               aria-label='Toggle menu'
+            >
+               <img src={isOpen ? "assets/close.svg" : "assets/menu.svg"} alt="toggle" className='w-6 h-6' />
             </button>
+
+            <nav className='sm:flex hidden'>
+               <NavItems />
+            </nav>
          </div>
 
       </div>
