105aed5e8b5aacf77ec31aaeb4db2fe5e3c2f51b
tiagosousa <tiagosousa.tams@hotmail.com>
2025-04-04 14:01:33 +0100

    new controls added to manipulate the image with leva

--- threejs-portfolio/src/sections/Hero.jsx
+++ threejs-portfolio/src/sections/Hero.jsx
@@ -3,8 +3,48 @@ import React, { Suspense } from 'react'
 import HackerRoom from '../components/HackerRoom'
 import CanvasLoader from '../components/CanvasLoader'
 import { Canvas } from '@react-three/fiber'
+import {Leva, useControls} from 'leva'
 
 const Hero = () => {
+   const x = useControls('HackerRoom',{
+      positionX: {
+         value: 2.5,
+         min: -10,
+         max: 10
+      },
+      positionY: {
+         value: 2.5,
+         min: -10,
+         max: 10
+      },
+      positionZ: {
+         value: 2.5,
+         min: -10,
+         max: 10
+      },
+      rotationX: {
+         value: 2.5,
+         min: -10,
+         max: 10
+      },
+      rotationY: {
+         value: 2.5,
+         min: -10,
+         max: 10
+      },
+      rotationZ: {
+         value: 2.5,
+         min: -10,
+         max: 10
+      },
+      scale: {
+         value: 2.5,
+         min: -10,
+         max: 10
+      }
+   })
+
+
   return (
     <section className='min-h-screen w-full flex-col relative'>
       <div className='w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3'>
@@ -15,11 +55,19 @@ const Hero = () => {
       </div>
 
       <div className='w-full h-full absolute inset-0'>
+          <Leva />
+
          <Canvas className='w-full h-full'>
             <Suspense fallback={<CanvasLoader />}>
 
                <PerspectiveCamera makeDefault position={[0, 0, 30]} />
-               <HackerRoom scale={0.07} position={[0, 0, 0]} rotation={[0, 280 ,0]} />
+               <HackerRoom 
+                  position={[2, -8, 2]} 
+                  rotation={[0, -Math.PI,0]} 
+
+                  scale={0.1}
+               />
+
                <ambientLight intensity={1} />
                <directionalLight position={[10,10,10]} intensity={0.5}/>
 
