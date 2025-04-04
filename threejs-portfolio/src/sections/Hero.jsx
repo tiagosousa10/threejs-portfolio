b444f83ca80633c44402b9c6760a735afa739c08
tiagosousa <tiagosousa.tams@hotmail.com>
2025-04-04 14:33:51 +0100

    react-logo animation

--- threejs-portfolio/src/sections/Hero.jsx
+++ threejs-portfolio/src/sections/Hero.jsx
@@ -7,6 +7,7 @@ import { Canvas } from '@react-three/fiber'
 import { useMediaQuery } from 'react-responsive'
 import {calculateSizes} from '../constants/index.js'
 import Target from '../components/Target.jsx'
+import ReactLogo from '../components/ReactLogo.jsx'
 
 const Hero = () => {
    // const controls = useControls('HackerRoom',{
@@ -78,6 +79,7 @@ const Hero = () => {
 
                <group>
                   <Target position={sizes.targetPosition}/>
+                  <ReactLogo position={sizes.reactLogoPosition} />
                </group>
 
                <ambientLight intensity={1} />
