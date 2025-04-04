994843b1dc67b7e6c6f4cae591d254cf299da8ca
tiagosousa <tiagosousa.tams@hotmail.com>
2025-04-04 14:22:58 +0100

    yoyo effect in target component

--- threejs-portfolio/src/components/Target.jsx
+++ threejs-portfolio/src/components/Target.jsx
@@ -1,5 +1,7 @@
 import { useGLTF } from '@react-three/drei'
 import React, { useRef } from 'react'
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
 
 const Target = (props) => {
    const targetRef = useRef();
@@ -7,9 +9,18 @@ const Target = (props) => {
       'https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/target-stand/model.gltf'
    )
 
+   useGSAP(() => {
+      gsap.to(targetRef.current.position, {
+         y: targetRef.current.position.y + 0.5,
+         duration: 1.5,
+         repeat: -1,
+         yoyo: true,
+      })
+   })
+
 
   return (
-    <mesh {...props} ref={targetRef}>
+    <mesh {...props} ref={targetRef} rotation={[0, Math.PI / 5,0]}>
       <primitive object={scene} />
     </mesh>
   )
