5545abb47c27e62e81580aa534fe86b8e58d1ccf
tiagosousa <tiagosousa.tams@hotmail.com>
2025-04-05 17:28:10 +0100

    globe library added and respective jsx in about section

--- threejs-portfolio/src/sections/About.jsx
+++ threejs-portfolio/src/sections/About.jsx
@@ -1,40 +1,70 @@
 import React from 'react'
+import Globe from 'react-globe.gl'
+import Button from '../components/Button'
 
 const About = () => {
   return (
-    <section className='c-space my-20'>
-      <div className='grid xl:grid-cols-3 xl:grid-rows-6 md:grid-cols-2 grid-cols-1 gap-5 h-full'>
-         {/*INTRO | HI, I'M TIAGO */}
-         <div className='col-span-2 xl:row-span-3'>
-            <div className='grid-container'>
-               <img src="/assets/grid1.png" alt="grid-1" className='w-full sm:h-[276px] h-fit object-contain' />
+   <section className="c-space my-20" id="about">
+      <div className="grid xl:grid-cols-3 xl:grid-rows-6 md:grid-cols-2 grid-cols-1 gap-5 h-full">
 
-               <div>
-                  <p className='grid-headtext'>Hi, I'm Tiago</p>
-                  <p className='grid-subtext'>With 1 year of experience, i have honed my skills in frontend and backend development, with a focus on animated 3D websites.</p>
-               </div>
+      {/*INTRO | HI */}
+
+        <div className="col-span-1 xl:row-span-3">
+          <div className="grid-container">
+            <img src="assets/grid1.png" alt="grid-1" className="w-full sm:h-[276px] h-fit object-contain" />
+
+            <div>
+              <p className="grid-headtext">Hi, I’m Tiago Sousa</p>
+              <p className="grid-subtext">
+                With 1 year of experience, I have honed my skills in both frontend and backend dev, creating dynamic
+                and responsive websites.
+              </p>
             </div>
-         </div>
+          </div>
+        </div>
+
          {/*TECH STACK */}
-         <div className='col-span-1 xl:row-span-3'>
-            <div className='grid-container'>
-               <img src="assets/grid2.png" alt="grid-2" className='w-full sm:w-[276px] h-fit object-contain' />
+        <div className="col-span-1 xl:row-span-3">
+          <div className="grid-container">
+            <img src="assets/grid2.png" alt="grid-2" className="w-full sm:h-[276px] h-fit object-contain" />
 
-               <div>
-                  <p className='grid-headtext'>Tech Stack</p>
-                  <p className='grid-subtext'>I specialize in JavaScript/TypeScript with a focus on React and Next.js ecosystems.</p>
-               </div>
+            <div>
+              <p className="grid-headtext">Tech Stack</p>
+              <p className="grid-subtext">
+                I specialize in a variety of languages, frameworks, and tools that allow me to build robust and scalable
+                applications
+              </p>
             </div>
-         </div>
+          </div>
+        </div>
 
-         <div className='col-span-1 xl:row-span-4'>
-            <div className='grid-container'>
-               <div className='rounded-3xl w-full sm:h-[326px] h-fit flex justify-center items-center'>
-
-               </div>
-
+         {/*GLOBE */}
+        <div className="col-span-1 xl:row-span-4">
+          <div className="grid-container">
+            <div className="rounded-3xl w-full sm:h-[326px] h-fit flex justify-center items-center">
+              <Globe
+                height={326}
+                width={326}
+                backgroundColor="rgba(0, 0, 0, 0)"
+                backgroundImageOpacity={0.5}
+                showAtmosphere
+                showGraticules
+                globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
+                bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
+                
+              />
             </div>
-         </div>
+            <div>
+              <p className="grid-headtext">I’m very flexible with time zone communications & locations</p>
+              <p className="grid-subtext">I&apos;m based in Rjieka, Croatia and open to remote work worldwide.</p>
+              <Button 
+                  name="Contact Me" 
+                  isBeam 
+                  containerClass="w-full mt-10" 
+               />
+            </div>
+          </div>
+        </div>
 
       </div>
     </section>
