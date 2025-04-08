75ee7d1bf8e85179d886c5a23ef136ee81a5d7ab
tiagosousa <tiagosousa.tams@hotmail.com>
2025-04-08 12:52:11 +0100

    mapping projects to show

--- threejs-portfolio/src/sections/Projects.jsx
+++ threejs-portfolio/src/sections/Projects.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import { myProjects } from '../constants'
 
 const Projects = () => {
+
+   const currentProject = myProjects[0]
+
   return (
     <section className='s-space my-20'>
       <p className='head-text'>My Work</p>
@@ -9,11 +12,32 @@ const Projects = () => {
       <div className='grid lg:grid-cols-2 grid-cols-1 mt-12 gap-5 w-full'>
          <div className='flex flex-col gap-5 relative sm:p-10 px-5 shadow-2xl shadow-black-200'>
             <div className='absolute top-0 right-0'>
-               <img src={myProjects[0].spotlight} alt="spotlight" className='w-full h-96 object-cover rounded-xl' />
+               <img src={currentProject.spotlight} alt="spotlight" className='w-full h-96 object-cover rounded-xl' />
             </div>
 
-            <div className='p-3 backdrop-filter backdrop-blur-3xl w-fit rounded-lg' style={myProjects[0].logoStyle}>
-               <img src={myProjects[0].logo} alt="logo" className='w-10 h-10 shadow-sm' />
+            <div className='p-3 backdrop-filter backdrop-blur-3xl w-fit rounded-lg' style={currentProject.logoStyle}>
+               <img src={currentProject.logo} alt="logo" className='w-10 h-10 shadow-sm' />
+            </div>
+
+            <div className='flex flex-col gap-5 text-white-600 my-5'>
+                  <p className='text-white text-2xl font-semibold animatedText'>{currentProject.title}</p>
+                  <p className='animatedText'>{currentProject.desc}</p>
+                  <p className='animatedText'>{currentProject.subdesc}</p>
+            </div>
+
+            <div className='flex items-center justify-between flex-wrap gap-5'>
+               <div className='flex items-center gap-3'>
+                  {currentProject.tags.map((tag,index) => (
+                     <div key={index} className='tech-logo'>
+                        <img src={tag.path} alt={tag.name} />
+                     </div>
+                  ))}
+               </div>
+
+               <a className='flex items-center gap-2 cursor-pointer text-white-600' href={currentProject.href} target='_blank' rel='noreferrer'>
+                  <p>Check Live Site</p>
+                  <img src="assets/arrow-up.png" alt="arrow" className='size-3' />
+               </a>
             </div>
          </div>
 
