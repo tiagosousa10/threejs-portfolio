d266e83792e4c0f47a63f6adaff86c81581fd8ef
tiagosousa <tiagosousa.tams@hotmail.com>
2025-04-08 16:51:58 +0100

    email service configurations added

--- threejs-portfolio/src/sections/Contact.jsx
+++ threejs-portfolio/src/sections/Contact.jsx
@@ -1,4 +1,5 @@
 import React, { useRef, useState } from 'react'
+import emailjs from '@emailjs/browser'
 
 const Contact = () => {
 
@@ -11,12 +12,38 @@ const Contact = () => {
       message: ''
    })
 
-   const handleChange = () => {
-
+   //reusable function to input data
+   const handleChange = ({target: {name, value}}) => {
+      setForm({...form, [name] : value})
    }
+   //service_yl1wher
 
-   const handleSubmit = () => {
+   //send email
+   const handleSubmit = async (e) => {
+      e.preventDefault()
 
+      setLoading(true)
+
+      try {
+         emailjs.send('service_2qp1re7', 'template_wid2tqz', {
+            name: form.name,
+            email: form.email,
+            message: form.message,
+            title: 'New Contact Message'
+         }, 
+            'lE5144Iu5V2OwYX1k' // public key
+         )
+
+         setLoading(false)
+         alert('Your message has been sent. Thank you!')
+
+      } catch(error) {
+         setLoading(false)
+         console.log(error)
+         alert('Something went wrong. Please try again.')
+      }
+
+   
    }
 
   return (
@@ -70,6 +97,12 @@ const Contact = () => {
                 placeholder="Share your thoughts or inquiries..."
               />
             </label>
+
+            <button className='field-btn' type='submit' disabled={loading}>
+               {loading ? 'Sending...' : 'Send Message'}
+
+               <img src="/assets/arrow-up.png" alt="arrow-up" className='field_btn-arrow' />
+            </button>
 
             </form>
          </div>
