0cbc8bbe3fd4670488f85b4e5f3712becc4fb095
tiagosousa <tiagosousa.tams@hotmail.com>
2025-04-08 19:01:39 +0100

    final adjustments

--- threejs-portfolio/src/sections/Experience.jsx
+++ threejs-portfolio/src/sections/Experience.jsx
@@ -1,63 +1,65 @@
-import { Canvas } from '@react-three/fiber'
-import React, { Suspense } from 'react'
-import { workExperiences } from '../constants'
-import { OrbitControls } from '@react-three/drei'
-import CanvasLoader from '../components/CanvasLoader'
-import Developer from '../components/Developer'
+import { Suspense, useState } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
 
-const Experience = () => {
+import Developer from '../components/Developer.jsx';
+import CanvasLoader from '../components/CanvasLoader.jsx';
+import { workExperiences } from '../constants/index.js';
+
+const WorkExperience = () => {
+  const [animationName, setAnimationName] = useState('idle');
+
   return (
-    <section className='c-space'>
-      <div className='w-full text-white-600'>
-         <h3 className='head-text'>
-            My Work Experience
-         </h3>
+    <section className="c-space my-20" id="work">
+      <div className="w-full text-white-600">
+        <p className="head-text">My Work Experience</p>
 
-         <div className='work-container'>
-            <div className='work-canvas'>
-               <Canvas>
-                  <ambientLight intensity={7} />
-                  <spotLight position={[10,10,10]} angle={0.15} penumbra={1}/>
-                  <directionalLight   position={[-10,10,10]} intensity={1} />
-                  <OrbitControls enableZoom={false} maxPolarAngle={Math.PI / 2} />
-                  <Suspense  fallback={<CanvasLoader />} >
-                  <Developer position-y={-3} scale={3} />
-                  </Suspense>
-               </Canvas>
+        <div className="work-container">
+          <div className="work-canvas">
+            <Canvas>
+              <ambientLight intensity={7} />
+              <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+              <directionalLight position={[10, 10, 10]} intensity={1} />
+              <OrbitControls enableZoom={false} maxPolarAngle={Math.PI / 2} />
 
+              <Suspense fallback={<CanvasLoader />}>
+                <Developer position-y={-3} scale={3} animationName={animationName} />
+              </Suspense>
+            </Canvas>
+          </div>
+
+          <div className="work-content">
+            <div className="sm:py-10 py-5 sm:px-5 px-2.5">
+              {workExperiences.map((item, index) => (
+                <div
+                  key={index}
+                  onClick={() => setAnimationName(item.animation.toLowerCase())}
+                  onPointerOver={() => setAnimationName(item.animation.toLowerCase())}
+                  onPointerOut={() => setAnimationName('idle')}
+                  className="work-content_container group">
+                  <div className="flex flex-col h-full justify-start items-center py-2">
+                    <div className="work-content_logo">
+                      <img className="w-full h-full" src={item.icon} alt="" />
+                    </div>
+
+                    <div className="work-content_bar" />
+                  </div>
+
+                  <div className="sm:p-5 px-2.5 py-5">
+                    <p className="font-bold text-white-800">{item.name}</p>
+                    <p className="text-sm mb-5">
+                      {item.pos} -- <span>{item.duration}</span>
+                    </p>
+                    <p className="group-hover:text-white transition-all ease-in-out duration-500">{item.title}</p>
+                  </div>
+                </div>
+              ))}
             </div>
-
-            <div className='work-content'>
-               <div className='sm:py-10 py-5 sm:px-5 px-2.5'>
-                  {workExperiences.map(({name, pos, duration, title, icon,animation, id}) => (
-                     <div key={id} className='work-content_container group'>
-                        <div className='flex flex-col h-full justify-start items-center py-2'>
-                           <div className='work-content_logo'>
-                              <img src={icon} alt="logo" className='w-full h-full' />
-                           </div>
-
-                           <div className='work-content_bar' />
-
-                        </div>
-
-                        <div className='sm:p-5 px-2.5 py-5'>
-                           <p className='font-bold text-white-800'>{name}</p>
-                           <p className='text-sm mb-5'>{pos} -- {duration}</p>
-                           <p className='group-hover:text-white transition ease-in-out duration-500' >{title}</p>
-
-                        </div>
-                     </div>
-                  ))}
-
-               </div>
-
-            </div>
-
-         </div>
-
+          </div>
+        </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Experience
+export default WorkExperience;
